604f9184bd61ad85eb7d6a0d2b2fafadf7b6ae2b
José Fernando <jf8065572@gmail.com>
2024-05-12 10:24:46 -0300

    Adicionando o título do jogo

--- index.py
+++ index.py
@@ -13,6 +13,10 @@ janela = pygame.display.set_mode(tamanho_janela)
 # Definindo o título da janela
 pygame.display.set_caption("Show do Milho")
 
+# Criando o título do jogo
+titulo = pygame.font.Font(None, 60).render("Show do Milho", True, (242, 250, 17))
+posicao_titulo = titulo.get_rect(center=(800 // 2, 100))  # Pegando a posição central do título
+
 # Loop principal do jogo
 rodando = True
 while rodando:
@@ -23,6 +27,9 @@ while rodando:
 
     # Preencher a tela com a cor de fundo
     janela.fill("#198536")
+
+    # Escrevendo o título na tela
+    janela.blit(titulo, posicao_titulo)
 
     # Atualizar a tela
     pygame.display.flip()
