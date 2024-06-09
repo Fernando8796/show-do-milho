2cd448c1e4763e28c78b50e57f50547a0b0ff0a8
José Fernando <jf8065572@gmail.com>
2024-06-09 18:13:05 -0300

    Alteração para deixar o jogo jogável

--- index.py
+++ index.py
@@ -18,6 +18,15 @@ pygame.display.set_caption("Show do Milho")
 # Definindo as cores dos botões
 cor_botao = (242, 250, 17)
 
+# Abrindo o arquivo
+with open('database.json', 'r') as arquivo:
+    dados = json.load(arquivo)
+
+# Pegando 13 perguntas aleatórias da base de dados
+perguntas = random.sample(dados, 13)
+questao = 0
+
+
 # Função para criar botões
 def criar_botao(texto, posicao, tamanho, cor):
     retangulo = pygame.Rect(posicao, tamanho)
@@ -43,28 +52,46 @@ def tela_inicial():
     # Escrevendo o título na tela
     janela.blit(titulo, posicao_titulo)
 
-    # Criando e retornando os botões
-    return {
-        "Jogar": criar_botao("Jogar", (250, 200), (300, 50), cor_botao),
-        "Ranking" : criar_botao("Ranking", (250, 300), (300, 50), cor_botao),
-        "Creditos": criar_botao("Créditos", (250, 400), (300, 50), cor_botao)
-    }
+    criar_botao("Jogar", (250, 200), (300, 50), cor_botao)
+    criar_botao("Ranking", (250, 300), (300, 50), cor_botao)
+    criar_botao("Créditos", (250, 400), (300, 50), cor_botao)
+        
 
 # Função que vai ser o modelo da tela jogar
 def tela_jogar():
 
-    # Criando o título do jogo
-    pergunta = pygame.font.Font(None, 30).render("Pergunta Teste", True, (242, 250, 17))
+    global questao # Declarada como global para evitar erro de escopo
 
-    # Escrevendo a pergunta na tela
-    janela.blit(pergunta, (50, 50))
+    # Criando a pergunta
+    numeroQuestao = pygame.font.Font(None, 30).render(f"Pergunta Nº {questao + 1}", True, (242, 250, 17))
+    janela.blit(numeroQuestao, (50, 50))
 
-    return {
-        "Resposta1": criar_botao("Resposta1", (90, 320), (300, 70), cor_botao), 
-        "Resposta2": criar_botao("Resposta2", (410, 320), (300, 70), cor_botao),
-        "Resposta3": criar_botao("Resposta3", (90, 410), (300, 70), cor_botao),
-        "Resposta4": criar_botao("Resposta4", (410, 410), (300, 70), cor_botao)
-    }
+    # Criando a pergunta
+    pergunta = pygame.font.Font(None, 30).render(perguntas[questao]["pergunta"], True, (242, 250, 17))
+    janela.blit(pergunta, (50, 80))
+
+    btn0 = criar_botao(perguntas[questao]["alternativas"][0], (90, 320), (300, 70), cor_botao)
+    btn1 = criar_botao(perguntas[questao]["alternativas"][1], (410, 320), (300, 70), cor_botao)
+    btn2 = criar_botao(perguntas[questao]["alternativas"][2], (90, 410), (300, 70), cor_botao)
+    btn3 = criar_botao(perguntas[questao]["alternativas"][3], (410, 410), (300, 70), cor_botao)
+
+    # Pegar os eventos dos botões, para ver se acertou ou não
+    for evento in pygame.event.get():
+        if evento.type == pygame.MOUSEBUTTONDOWN and evento.button == 1:
+            posicao_mouse = pygame.mouse.get_pos()
+
+            if botao_clicado(btn0, posicao_mouse):
+                if perguntas[questao]["resposta"] == 0:
+                    questao += 1
+            elif botao_clicado(btn1, posicao_mouse):
+                if perguntas[questao]["resposta"] == 1:
+                    questao += 1
+            elif botao_clicado(btn2, posicao_mouse):
+                if perguntas[questao]["resposta"] == 2:
+                    questao += 1
+            elif botao_clicado(btn3, posicao_mouse):
+                if perguntas[questao]["resposta"] == 3:
+                    questao += 1
 
 # Dicionário para as telas disponíveis
 telas = {
@@ -75,43 +102,31 @@ telas = {
 # Começar com a tela inicial, mas ir mudando com o tempo
 tela_atual = "Inicial"
 
-# Abrindo o arquivo
-with open('database.json', 'r') as arquivo:
-    dados = json.load(arquivo)
-
-# Pegando 13 perguntas aleatórias da base de dados
-perguntas = random.sample(dados, 13)
-
 # Loop principal do jogo
 rodando = True
 while rodando:
+
     # Loop para manipular os eventos
     for evento in pygame.event.get():
         # Manipulando evento de saída do jogo
         if evento.type == pygame.QUIT:
             rodando = False
-        # Se o evento foi o clique do botão esquerdo 
-        elif evento.type == pygame.MOUSEBUTTONDOWN and evento.button == 1:
-            # Pegando a posição do mouse
+        if evento.type == pygame.MOUSEBUTTONDOWN and evento.button == 1:
             posicao_mouse = pygame.mouse.get_pos()
-            # Cndições para trocar de tela
-            if botao_clicado(botoes["Jogar"], posicao_mouse):
-                tela_atual = "Jogar"
-            elif botao_clicado(botoes["Ranking"], posicao_mouse):
-                print("Botão ranking clicado!")
-            elif botao_clicado(botoes["Creditos"], posicao_mouse):
-                print("Botão crédito clicado!")
+
+            if tela_atual == "Inicial":
+                if botao_clicado(pygame.Rect((250, 200), (300, 50)), posicao_mouse):
+                    tela_atual = "Jogar"
     
     # Tela de fundo do jogo
     janela.fill("#198536")
 
     # Chamando a função da tela atual
-    botoes = telas[tela_atual]()
-    # ^-- Vai receber os botoes da tela atual
+    telas[tela_atual]()
     
     # Atualizar a tela
     pygame.display.flip()
-    clock.tick(10)  # limitar o FPS para 10
+    clock.tick(30)  # limitar o FPS para 10
 
 # Encerrar o jogo
 pygame.quit()
